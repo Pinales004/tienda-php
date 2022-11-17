3e5bf3c979e9e4abd9fdceff247c65d7d5e3e89a
omar01519 <115951763+omar01519@users.noreply.github.com>
2022-11-17 12:16:30 -0400

    banner principar relleno con la base de datos
    falta mejorar los estilos y atributos de las imagenes y tabla

--- index.php
+++ index.php
@@ -617,14 +617,34 @@ include("head.php");
 				</div>
 			</div>
 
+
+
+
+
+
+						
+ 				<table style="width: 125%">
+ 						<tr>
+
+
+						<?php
+						$columna=0;
+						$fila=0;
+                        include("conexion/db.php");
+                        $query = "SELECT * FROM perfumes";
+                        $resultado = $conn->query($query);
+                        while($row = $resultado->fetch_assoc()){
+                        echo '<td>';
+                   		 ?>
+
 			<div class="row isotope-grid">
 				<div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item women">
 					<!-- Block2 -->
 					<div class="block2">
 						<div class="block2-pic hov-img0">
-							<img src="images/Bvlgari Man In Black Bvlgari375x500.webp" alt="IMG-PRODUCT">
-
-							<a href="#" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
+							<img   height="200px"  src="data:image/jpg;base64,<?php echo base64_encode($row['imagen']);?>"  alt="IMG-PRODUCT"/>
+							
+							<a href="#" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">alt="IMG-PRODUCT"
 								Vistazo rápido
 							</a>
 						</div>
@@ -632,11 +652,11 @@ include("head.php");
 						<div class="block2-txt flex-w flex-t p-t-14">
 							<div class="block2-txt-child1 flex-col-l ">
 								<a href="product-detail.html" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
-									Bvlgari Man In Black Bvlgari
+									<?php echo $row['nombrePerfume']; ?>
 								</a>
 
 								<span class="stext-105 cl3">
-									$16.64
+									 $<?php echo $row['Precio']; ?>    
 								</span>
 							</div>
 
@@ -649,6 +669,63 @@ include("head.php");
 						</div>
 					</div>
 				</div>
+			
+							
+
+			<?php 
+			echo '</td>';
+			$fila=$fila+1;
+			if($fila==4){
+				
+				echo '</tr>';
+				 
+				$fila=0;
+				$columna=$columna+1;
+				if($columna==6){
+					break;
+				}
+				}
+				} ?>
+
+			</table>
+
+
+
+
+
+<div class="row isotope-grid">
+				<div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item women">
+					<!-- Block2 -->
+					<div class="block2">
+						<div class="block2-pic hov-img0">
+							<img src="images/Allure Sensuelle Chanel.webp" alt="IMG-PRODUCT">
+							
+							<a href="#" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">alt="IMG-PRODUCT"
+								Vistazo rápido
+							</a>
+						</div>
+
+						<div class="block2-txt flex-w flex-t p-t-14">
+							<div class="block2-txt-child1 flex-col-l ">
+								<a href="product-detail.html" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
+									Babycat Yves Saint Laurent
+								</a>
+
+								<span class="stext-105 cl3">
+									 $35.31   
+								</span>
+							</div>
+
+							<div class="block2-txt-child2 flex-r p-t-3">
+								<a href="#" class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
+									<img class="icon-heart1 dis-block trans-04" src="images/icons/icon-heart-01.png" alt="ICON">
+									<img class="icon-heart2 dis-block trans-04 ab-t-l" src="images/icons/icon-heart-02.png" alt="ICON">
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+
 
 				<div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item women">
 					<!-- Block2 -->
@@ -777,6 +854,9 @@ include("head.php");
 						</div>
 					</div>
 				</div>
+
+
+
 
 				<div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item watches">
 					<!-- Block2 -->
@@ -1130,6 +1210,8 @@ include("head.php");
 					</div>
 				</div>
 			</div>
+
+
 
 			<!-- Load more -->
 			<div class="flex-c-m flex-w w-full p-t-45">
